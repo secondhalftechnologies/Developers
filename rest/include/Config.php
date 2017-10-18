730df7c299cbd41d1840fda1faf76ab1effe9abe
Ejaz Ansari <ejaz@edmission.in>
2017-10-18 15:52:04 +0530

    Slim Rest Api init

---
+++ rest/include/Config.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+/** Database config */
+define('DB_USERNAME', 'root');  
+define('DB_PASSWORD', '');  
+define('DB_HOST', 'localhost');  
+define('DB_NAME', 'sqyard_2017');
+
+
+/** Debug modes */
+define('PHP_DEBUG_MODE', true);  
+define('SLIM_DEBUG', true);
+

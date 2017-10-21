42842cde325779111b549f0e5332a1028e007144
Ejaz Ansari (Ejaz47) <ejjuak47@gmail.com>
2017-10-22 01:46:01 +0530

    Sample GET, POST, PUT for kyc_knowlwdge

--- rest/v1/index.php
+++ rest/v1/index.php
@@ -45,7 +45,7 @@ if(SLIM_DEBUG){
 	if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
 	    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-	        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+	        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
 
 	    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
 	        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
@@ -71,6 +71,7 @@ $app->get('/test/:name', function ($name) {
 
 require './routes/Authentication.php';
 require './routes/Farmers.php';
+require './routes/Kyc_knowledge.php';
 
 
 // Run Slim
---
+++ rest/v1/routes/Kyc_knowledge.php
@@ -0,0 +1,104 @@
+<?php 
+
+
+	/**
+	 * Get kyc_knowledge by farmer id
+	 * It is just an example you will have to get the current agent_id then list his farmer list
+	 * 'authenticate' parameter is a function in Utils.php file where we are checking authorization 
+	 */
+	$app->get('/kyc_knowledge/:id', 'authenticate', function($id) use ($app) {
+
+		if (isset($id)) {
+
+			//will have to fetch data from database
+			//eg. select * from kyc_knowledge where farmer_id = [id]
+			$kyc_knowledge = [
+				"f2_edudetail" => 'illiterate',
+				"f2_participation" => 'no',
+				"f2_points" => '2.33',
+				"f2_proficiency" => 'read only',
+			];
+
+			$response["success"] = true;
+			$response["data"] = $kyc_knowledge;
+
+		}
+		else{
+			$response["success"] = false;
+			$response["message"] = " parameter Id is missing!";
+		}
+
+		echoResponse(200, $response);
+	});
+
+
+	/**
+	 *  Add new Farmer kyc_knowledge to the data base with Post method
+	 *  The following post method will create a kyc_knowledge row in data base
+	 * 'authenticate' parameter is a function in Utils.php file where we are checking authorization 
+	 */
+	$app->post('/kyc_knowledge', 'authenticate', function() use ($app){
+		verifyRequiredParams([ 
+								"f2_edudetail",
+								"f2_participation",
+								"f2_points",
+								"f2_proficiency" ]); //provide a list of required parametes
+		$response = [];
+		$farmer_data = $app->request->post(); //fetching the post data into variable
+		
+		// ------------------------
+		// Do Validation here
+		// ------------------------
+
+		//Do database instertion here
+		$successInsertion = true;
+
+
+		if ($successInsertion != false) {
+			$response["success"] = true;
+			$response["message"] = "applicant knowledge added successfully!";
+		} else {
+			$response["success"] = false;
+			$response["message"] = "Failed to Add applicant knowledge. Please try again";
+		}
+		echoResponse(201, $response);
+	});
+
+
+	/**
+	 *  Update kyc_knowledge to the data base with Put method
+	 *  The following put method will update a kyc_knowledge row in data base
+	 * 'authenticate' parameter is a function in Utils.php file where we are checking authorization 
+	 */
+	$app->put('/kyc_knowledge', 'authenticate', function() use ($app){
+		verifyRequiredParams([ 
+								"f2_edudetail",
+								"f2_participation",
+								"f2_points",
+								"f2_proficiency" ]); //provide a list of required parametes
+		$response = [];
+		$farmer_data = $app->request->put(); //fetching the post data into variable
+		
+		// ------------------------
+		// Do Validation here
+		// ------------------------
+
+		//Do database instertion here
+		$successUpdation = true;
+
+		
+		if ($successUpdation != false) {
+			$response["success"] = true;
+			$response["message"] = "applicant knowledge updated successfully!";
+		} else {
+			$response["success"] = false;
+			$response["message"] = "Failed to Add applicant knowledge. Please try again";
+		}
+		echoResponse(200, $response);
+	});
+
+
+
+
+
+ ?>
